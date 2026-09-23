0bb12496f4bc6f129be9aaee3020c1a67a88a43c
baderalhindi <baderayman43@gmail.com>
2026-09-23 19:25:50 +0300

    chore(test): remove scratch runner scripts (ST-290)

--- .lint-ann.ps1
+++
@@ -1,3 +0,0 @@
-﻿$base = 'C:\Users\User\Desktop\Studafy\studafy'
-$pkgs = bunx -c $base turbo run lint --filter=@studafy/announcements --continue=false 2>&1 | Out-String
-Write-Output ($pkgs.Trim().Split([char]10) | Select-Object -Last 20)
